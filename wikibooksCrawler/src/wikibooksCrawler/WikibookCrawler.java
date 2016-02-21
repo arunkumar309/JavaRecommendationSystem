695c12c473b6542ba5aa9a2f6305a0421182f531
arunkumar309 <arunkumar309@gmail.com>
2016-02-21 01:01:50 -0700

    Updated to crawl all links, to-do: write to file

--- wikibooksCrawler/src/wikibooksCrawler/WikibookCrawler.java
+++ wikibooksCrawler/src/wikibooksCrawler/WikibookCrawler.java
@@ -25,63 +25,137 @@ public class WikibookCrawler {
 		String url = "https://en.wikibooks.org/wiki/Java_Programming";
 		LinkedHashMap<String, String> topicsMap = new LinkedHashMap<String, String>();
 		HashMap<String, LinkedHashMap<String, String>> pages = new HashMap<String, LinkedHashMap<String, String>>();
+		String pageClassifier;
 		getLinks(url, topicsMap);
+
 		for(Map.Entry<String, String> entry:topicsMap.entrySet()){ 
 			String link = entry.getValue();
-			getPageStart(link, pages);
-			getText(link, pages);
+			pageClassifier = getPageStart(link, pages);
+			if (!pageClassifier.equals("noscript"))
+				getText(link, pages, pageClassifier);
 		}
 		print("%d", pages.size());
-/*	 
-		getPageStart(url+"/Statements", pages);
-		getText(url+"/Statements", pages);
+
+	 
+/*		pageClassifier = getPageStart(url+"/3D_Programming", pages);
+		if (!pageClassifier.equals("noscript"))
+			getText(url+"/3D_Programming", pages, pageClassifier);
 		print("%d %s", pages.size() , pages.toString());
 */	
 	}
 	
-	private static void getPageStart(String url,HashMap<String, LinkedHashMap<String, String>> hp) throws IOException {
+	private static String getPageStart(String url,HashMap<String, LinkedHashMap<String, String>> hp) throws IOException {
 		int index = url.lastIndexOf("/");
 		String title = url.substring(index+1);
+		boolean flag = true;
+		boolean divFlag = false;
 		print("Fetching pageStart: %s", title);
 		Document doc = Jsoup.connect(url).get();
+		String pageClassifier = null;
 		Elements contents = doc.select("#mw-content-text > table");
+		if (contents.size() == 0){
+			divFlag = true;
+			contents = doc.select("#mw-content-text > div > table");
+		}
 		LinkedHashMap<String, String> tempMap = new LinkedHashMap<String, String>();
 		for(Element content : contents){
 			StringBuilder sb = new StringBuilder();
-			if (content.hasClass("wikitable")){
+			if (content.hasClass("metadata")){
+				pageClassifier = "noscript";
 				content = content.nextElementSibling();
-				String text;
-				while(!content.nextElementSibling().tagName().equals("h2")){
-					text = content.text();
-					if(text.startsWith("Code section") || text.startsWith("Code listing")){
-						int indexTemp = text.lastIndexOf(": ");
-						text = text.substring(indexTemp+2);
-					}
-					sb.append(text+"\n");
+				while(!content.tagName().equals("noscript")){
+					sb.append(content.text()+"\n");
 					content = content.nextElementSibling();
 				}
-				sb.append(content.text()+"\n");
 				tempMap.put("introduction", sb.toString());
+			}
+			else if (content.hasClass("wikitable") && content.hasAttr("style") && flag){
+				flag = false;
+				Element temp = content.nextElementSibling();
+				content = content.nextElementSibling();
+				String text;
+				if(divFlag){
+					Element lastElement = temp.lastElementSibling();
+					while(temp.hasText()){
+						sb.append(temp.text()+"\n");
+						if(temp != lastElement)
+							temp = temp.nextElementSibling();
+						else
+							break;
+					}
+					pageClassifier = "h2";
+					tempMap.put("introduction", sb.toString());
+				}
+				else{
+					while(!temp.nextElementSibling().tagName().equals("h2") && !temp.nextElementSibling().tagName().equals("h3") && !temp.nextElementSibling().tagName().equals("noscript")){
+						temp = temp.nextElementSibling();
+					}
+					pageClassifier  = temp.nextElementSibling().tagName();
+					switch(pageClassifier){
+						case "h2":{
+							while(!content.nextElementSibling().tagName().equals("h2")){
+								text = content.text();
+								if(text.startsWith("Code section") || text.startsWith("Code listing")){
+									int indexTemp = text.lastIndexOf(": ");
+									text = text.substring(indexTemp+2);
+								}
+								sb.append(text+"\n");
+								content = content.nextElementSibling();
+							}
+							sb.append(content.text()+"\n");
+	
+							break;
+						}
+						case "h3":{
+							sb.append(content.text()+"\n");
+							break;
+	
+						}
+						case "noscript":{
+							while(!content.nextElementSibling().nextElementSibling().tagName().equals("noscript")){
+								text = content.text();
+								if(text.startsWith("Code section") || text.startsWith("Code listing")){
+									int indexTemp = text.lastIndexOf(": ");
+									text = text.substring(indexTemp+2);
+								}
+								sb.append(text+"\n");
+								content = content.nextElementSibling();
+								break;
+							}
+						}
+						tempMap.put("introduction", sb.toString());
+					}
+				}
 			}
 		}
 		hp.put(title, tempMap);
+		return pageClassifier;
 	}
 
-	private static void getText(String url, HashMap<String, LinkedHashMap<String, String>> hp) throws IOException {
+	private static void getText(String url, HashMap<String, LinkedHashMap<String, String>> hp, String pageClassifier) throws IOException {
 		int index = url.lastIndexOf("/");
 		String title = url.substring(index+1);
 		print("Fetching Text: %s", title);
 		Document doc = Jsoup.connect(url).get();
 		Elements contents = doc.select("#mw-content-text > p");
-		Elements subHeadings = doc.select("#mw-content-text > h2");
+		Elements subHeadings = doc.select("#mw-content-text >" + pageClassifier);
 		Elements codes = doc.getElementsByClass("mw-highlight");
 		LinkedHashMap<String, String> tempMap = hp.get(title);
 		for(Element subHeading: subHeadings){
 			int len = subHeading.text().length();
-			String subTopic = subHeading.text().substring(0, len-6);
+			int pos;
+			String subTopic = null;
+			pos = subHeading.text().indexOf("[Edit]");
+			if (pos != -1)
+				subTopic = subHeading.text().substring(0, pos);
+			else if(pos == -1){
+				pos = subHeading.text().indexOf("Edit");
+				if (pos != -1)
+					subTopic = subHeading.text().substring(0, pos);
+			}
 			String text;
 			StringBuilder sb = new StringBuilder();
-			while(!subHeading.nextElementSibling().tagName().equals("h2") && subHeading.nextElementSibling().hasText()){
+			while(!subHeading.nextElementSibling().tagName().equals(pageClassifier) && subHeading.nextElementSibling().hasText()){
 				text = subHeading.nextElementSibling().text();
 				if(text.startsWith("Code section") || text.startsWith("Code listing")){
 					int indexTemp = text.lastIndexOf(": ");
@@ -128,6 +202,8 @@ public class WikibookCrawler {
 				String link = temp.attr("abs:href");
 				if (title.equals("Statements"))
 					flag = true;
+				if (title.equals("Links"))
+					flag = false;
 				if (hp.containsKey(title) == false && flag) {
 					hp.put(title, link);
 				}
